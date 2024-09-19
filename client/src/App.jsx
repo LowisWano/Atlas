94fc2cade087e2a68a620c46abe0ccd939eacff4
wiswiss <luisandreiouano@gmail.com>
2024-09-19 22:48:12 +0800

    feat: create navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from "@/components/mode-toggle"
-import { H1, P } from "./components/Typography"
+import { Outlet } from 'react-router-dom';
+import Navbar from "./components/layouts/navbar";
 
 function App() {
 
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ModeToggle/>
-        <H1>Atlas</H1>
-        <P>
-          An RPG style task management app that gamifies your goals and tasks. Earn exp, gold, and rank points as you accomplish tasks to level up your character and increase your rank!
-        </P>
+        <Navbar />
+          <main>
+            {/* Child routes (like Login) will be rendered here */}
+            <Outlet />
+          </main>
 
-    </ThemeProvider>
+      </ThemeProvider>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
+// pages
+import Dashboard from './pages/dashboard/index.jsx';
+import Login from './pages/login/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      }
+    ]
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
