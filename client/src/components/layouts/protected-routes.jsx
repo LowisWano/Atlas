b20a547949b7c92fe5c2e2d69d713370500cf23d
wiswiss <laeouano@gmail.com>
2024-10-08 12:42:35 +0800

    feat: implement protected routes

---
+++ client/src/components/layouts/protected-routes.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoutes = () => {
+  // disable protected route 
+	const localStorageToken = true;
+
+  // comment this line if you want to temporarily disable protected routes
+	// const localStorageToken = localStorage.getItem("token");
+
+	return localStorageToken ? <Outlet /> : <Navigate to="/login"  replace />;
+};
+
+export default ProtectedRoutes;
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -3,6 +3,8 @@ import Dashboard from './pages/dashboard/index.jsx';
 import Login from './pages/login/index.jsx';
 import Signup from './pages/signup/index.jsx'
 import ErrorPage from './components/layouts/error-page.jsx';
+import ProtectedRoutes from './components/layouts/protected-routes.jsx';
+import Calendar from './pages/calendar/index.jsx';
 
 const routes = [
   {
@@ -11,8 +13,18 @@ const routes = [
     errorElement: <ErrorPage />,
     children: [
       {
-        index: true,
-        element: <Dashboard />,
+        element: <ProtectedRoutes />,  // Protects all nested routes
+        children: [
+          {
+            index: true,
+            element: <Dashboard />,
+          },
+          {
+            path: "calendar",
+            element: <Calendar />,
+          }
+          // Add more protected routes here
+        ],
       },
       {
         path: "login",
