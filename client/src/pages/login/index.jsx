dcf0b700a53c39118b7197fd71f1173ec3e76028
wiswiss <laeouano@gmail.com>
2024-10-13 01:03:07 +0800

    nit: uncomment redirect to dashboard if user is already logged in

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -5,12 +5,12 @@ import { useNavigate } from "react-router-dom";
 export default function Login() {
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const cachedUser = JSON.parse(localStorage.getItem("token"));
-  //   if(cachedUser){
-  //     navigate('/');
-  //   } 
-  // }, [])
+  useEffect(() => {
+    const cachedUser = JSON.parse(localStorage.getItem("token"));
+    if(cachedUser){
+      navigate('/');
+    } 
+  }, [])
   
   return (
     <div className='flex items-center justify-center min-h-screen'>
