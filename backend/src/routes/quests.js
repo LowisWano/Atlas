765e2d54872c9797d692bbcba6dd7e099f649884
wiswiss <laeouano@gmail.com>
2024-10-25 16:55:15 +0800

    feat: implement require authorization header for GET requests to a player's quests

--- backend/src/routes/quests.js
+++ backend/src/routes/quests.js
@@ -2,7 +2,7 @@ const questsRouter = require('express').Router()
 const quests = require('../controllers/quests.controller')
 const { userExtractor } = require('../utils/middleware')
 
-questsRouter.get('/:id/quests', quests.getPlayerQuests)
+questsRouter.get('/:id/quests', userExtractor, quests.getPlayerQuests)
 questsRouter.post('/:id/quests', userExtractor, quests.createQuest)
 
 module.exports = questsRouter
