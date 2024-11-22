bafbb620a42cd2048b0c38e1e21c5149f2dccd67
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-22 21:40:22 +0800

    feat: added items route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const authRouter = require("./auth");
 const helloRouter = require("./hello");
 const questsRouter = require("./quests");
+const shopRouter = require("./items");
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/hello", helloRouter);
 rootRouter.use("/player", questsRouter);
+rootRouter.use("/items", shopRouter);
 
 module.exports = rootRouter;
