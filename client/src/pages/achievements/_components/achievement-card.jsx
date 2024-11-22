98a5a79cc5e3f408c58e864b7c75b5caea5d6b67
helpmegetoutofthiscodinghell <117290310+helpmegetoutofthiscodinghell@users.noreply.github.com>
2024-11-22 19:56:22 +0800

    nit: Added reward sprites

--- client/src/pages/achievements/_components/achievement-card.jsx
+++ client/src/pages/achievements/_components/achievement-card.jsx
@@ -1,6 +1,8 @@
 import { CardHeader, CardContent, CardFooter } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 
+
+
 const AchievementCard = ({ achievement, isFirst, isLast }) => (
     <div
         className={`p-4 flex items-center border border-gray-700 
@@ -12,7 +14,7 @@ const AchievementCard = ({ achievement, isFirst, isLast }) => (
         <div className="flex-1">
             <CardHeader className="text-xl font-semibold p-1">{achievement.title}</CardHeader>
             <CardContent className="text-gray-600 p-1">{achievement.description}</CardContent>
-            <CardFooter className="text-sm text-gray-400 p-1"> {achievement.rewardGold} Exp: {achievement.rewardExp} </CardFooter>
+            <CardFooter className="text-sm text-gray-400 p-1"><img src="sprites/GoldCoins-Sprites.png"/>{achievement.rewardGold} <img src="sprites/Experience-Sprites.png"/> {achievement.rewardExp} </CardFooter>
         </div>
         {/* <div className="ml-auto">
             <Checkbox id={`achievement-${achievement.id}`} checked={achievement.status === 1} className="w-10 h-10" readOnly />
