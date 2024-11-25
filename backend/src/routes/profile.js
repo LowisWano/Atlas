0c8a5923d215eeedf3e0d1f5bb5169bc9f99741c
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-25 22:11:02 +0800

    feat: added a new player route for owned items

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,9 +1,11 @@
 const profileRouter = require("express").Router({ mergeParams: true });
 const {
   getPlayerProfileController,
+  getPlayerPurchases,
 } = require("../controllers/profile.controller")
 const { tokenValidator } = require("../utils/middleware");
 
 profileRouter.get("/", tokenValidator, getPlayerProfileController);
+profileRouter.get("/items", tokenValidator, getPlayerPurchases);
 
 module.exports = profileRouter;
