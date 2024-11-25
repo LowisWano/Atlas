16ba15b46357a21aec8ed9cedaa4cbca9434109c
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-25 20:32:03 +0800

    feat: added useUserStore to shop page

--- client/src/pages/shop/index.jsx
+++ client/src/pages/shop/index.jsx
@@ -1,7 +1,11 @@
 import ShopHeader from "./components/ShopHeader";
 import ShopItems from "./components/ShopItems";
+import { useUserStore } from "@/hooks/auth-hooks";
+import { H1, P } from "@/components/Typography";
 
 export default function Shop() {
+  const { user } = useUserStore()
+  console.log(user)
   return (
     <div className="p-10">
       <ShopHeader />
