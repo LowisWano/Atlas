ea3c9d88ad29be7cffc2f0b770f17768638cb252
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-25 20:21:41 +0800

    feat: added controllers for getting Items and gettingPlayerItems

--- backend/src/routes/items.js
+++ backend/src/routes/items.js
@@ -1,16 +1,10 @@
 // items.js (routes)
 const itemsRouter = require("express").Router();
-const itemsController = require("../controllers/items.controller");
+const itemsController = require("../controllers/items.controller"); 
 const { tokenValidator } = require("../utils/middleware");
 
-// Search and filter routes
-itemsRouter.post("/search", tokenValidator, itemsController.searchItems);
-itemsRouter.get("/rarity/:rarity", tokenValidator, itemsController.getItemsByRarity);
-itemsRouter.get("/player/:playerId", tokenValidator, itemsController.getPlayerItems);
-
-// Basic CRUD routes
 itemsRouter.get("/", tokenValidator, itemsController.getItems);
-itemsRouter.get("/:id", tokenValidator, itemsController.getItem);
+itemsRouter.get("/player/:playerId", tokenValidator, itemsController.getPlayerItems);
 itemsRouter.post("/purchase", tokenValidator, itemsController.purchaseItem);
 
 module.exports = itemsRouter;
