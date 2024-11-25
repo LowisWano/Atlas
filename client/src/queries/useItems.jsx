e6bfc9b825950f778960f6929f60e479a75d1211
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-25 22:49:09 +0800

    feat: added a query file for items

---
+++ client/src/queries/useItems.jsx
@@ -0,0 +1,80 @@
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+  } from '@tanstack/react-query'
+  import { getUserItems} from '@/services/items.service';
+  import { useUserStore } from "@/hooks/auth-hooks";
+  import { useToast } from "@/hooks/use-toast"
+  
+  export function useItems() {
+    const { user } = useUserStore();
+    const queryClient = useQueryClient();
+    const { toast } = useToast();
+  
+    const { isPending, error, data } = useQuery({
+      queryKey: ['items'],
+      queryFn: () => getUserItems(user.token),
+      refetchOnWindowFocus: false
+    });
+    
+    function getItems() {
+      return {
+        isPending,
+        error,
+        data,
+      }
+    }
+  
+    // const createQuestMutation = useMutation({
+    //   mutationFn: (newQuest) => createQuest(user.user.id, user.token, newQuest),
+    //   onSuccess: async () => {
+    //     await queryClient.invalidateQueries({ queryKey: ['quests'] });
+    //     toast({
+    //       title: "Quest Created!",
+    //       description: "Your new quest has been added successfully.",
+    //     });
+    //   }
+    // });
+  
+    // const createQuestMutate = async (questEntry) => {
+    //   createQuestMutation.mutate(questEntry);
+    // }
+  
+    // const deleteQuestMutation = useMutation({
+    //   mutationFn: (quest) => deleteQuest(user.user.id, user.token, quest.id),
+    //   onSuccess: async (data, quest) => {
+    //     await queryClient.invalidateQueries({ queryKey: ['quests'] });
+    //     toast({
+    //       title: "Quest Deleted.",
+    //       description: `${quest.title} has been deleted successfully.`,
+    //     });
+    //   }
+    // })
+    
+    // const deleteQuestMutate = async (quest) => {
+    //   deleteQuestMutation.mutate(quest);
+    // }
+  
+    // const editQuestMutation = useMutation({
+    //   mutationFn: ({ questId, editedQuest }) => editQuest(user.user.id, user.token, questId, editedQuest),
+    //   onSuccess: (data, variables) => {
+    //     queryClient.setQueryData(['quests'], (oldData) => {
+    //       return oldData.map(quest => 
+    //         quest.id === variables.questId ? data : quest
+    //       );
+    //     });
+    //   }
+    // })
+  
+    // const editQuestMutate = async (questId, editedQuest) => {
+    //   editQuestMutation.mutate({ questId, editedQuest });
+    // }
+  
+    return {
+      getItems,
+    //   createQuestMutate,
+    //   deleteQuestMutate,
+    //   editQuestMutate
+    }
+  }
