0b142d09d8d9cbd7a28705011490714474741fe1
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-27 00:11:01 +0800

    feat: added a new path for purcheses

--- backend/src/routes/player.js
+++ backend/src/routes/player.js
@@ -1,8 +1,10 @@
 const playerRouter = require("express").Router();
 const questsRouter = require("./quests");
 const profileRouter = require("./profile")
+const purchasesRouter = require("./purchases")
 
 playerRouter.use("/:id/quests", questsRouter);
 playerRouter.use("/:id/profile", profileRouter);
+playerRouter.use("/:id/purchase", purchasesRouter);
 
 module.exports = playerRouter
