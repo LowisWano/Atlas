30f4dad1c4cc2934cf59b53dad7da2f2862e713d
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-27 23:34:29 +0800

    feat: compensate the fix in services

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,11 +1,8 @@
 const profileRouter = require("express").Router({ mergeParams: true });
-const {
-  getPlayerProfileController,
-  getPlayerPurchases,
-} = require("../controllers/profile.controller")
+const { getPlayerProfileController, getPlayerPurchases } = require("../controllers/profile.controller");
 const { tokenValidator } = require("../utils/middleware");
 
 profileRouter.get("/", tokenValidator, getPlayerProfileController);
-profileRouter.get("/items", tokenValidator, getPlayerPurchases);
+profileRouter.get("/items", tokenValidator, getPlayerPurchases); // Add this route
 
 module.exports = profileRouter;
