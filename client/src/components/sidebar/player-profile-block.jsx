b522dc1c8f7b09ba924a59c48f51e3ad9277a6ae
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-11 06:12:35 +0800

    feat: updated player profile section

--- client/src/components/sidebar/player-profile-block.jsx
+++ client/src/components/sidebar/player-profile-block.jsx
@@ -4,9 +4,6 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
 
 import {
@@ -15,46 +12,62 @@ import {
   AvatarImage,
 } from "@/components/ui/avatar"
 
-import { Card } from "../ui/card";
-
 import { useUserStore } from "@/hooks/auth-hooks";
 import { Progress } from "@/components/ui/progress"
-import { useState, useEffect } from "react";
-import {
-  Collapsible,
-} from "@/components/ui/collapsible"
+import { usePlayer } from "@/queries/usePlayer";
+import { useSidebar } from "@/components/ui/sidebar";
 
 export default function PlayerProfileBlock() {
-  const [progress, setProgress] = useState(13)
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500)
-    return () => clearTimeout(timer)
-  }, [])
-  
+  const { token, user } = useUserStore();
+  const { getPlayerData } = usePlayer();
+  const { playerInfo, userInfo, isLoading } = getPlayerData();
+  const { state } = useSidebar();
+
+  if (isLoading) {
+    return (
+      <SidebarGroup>
+        <SidebarGroupLabel>Profile</SidebarGroupLabel>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton className="flex h-20">
+              Loading...
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroup>
+    );
+  }
+
+  const expProgress = (playerInfo?.experience || 0) / (playerInfo?.level * 1000) * 100;
+
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Profile</SidebarGroupLabel>
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton className="flex h-20">
+          <SidebarMenuButton className="flex h-16">
             <div>
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" alt="avatar" />
-                <AvatarFallback>CN</AvatarFallback>
+              <Avatar className={state === "collapsed" ? "h-7 w-7" : ""}>
+                <AvatarImage src={playerInfo?.profilePic || "https://github.com/shadcn.png"} alt="avatar" />
+                <AvatarFallback>{userInfo?.name?.charAt(0).toUpperCase() || "U"}</AvatarFallback>
               </Avatar>
             </div>
             <div className="flex flex-col gap-[5px]">
-              <h1 className="text-label tracking-wider"><strong>Luis Andrei Ouano</strong></h1>
-              <p className="text-label tracking-wider">Level 1 • Copper</p>
-              <Progress value={progress} className="h-[7px]" />
+              <h1 className="text-label tracking-wider">
+                <strong>{userInfo?.name || "Unknown"}</strong>
+              </h1>
+              <p className="text-label tracking-wider">
+                Level {playerInfo?.level || 1} • {playerInfo?.adventurerRank?.charAt(0).toUpperCase() + playerInfo?.adventurerRank?.slice(1).toLowerCase() || "Unknown"}
+              </p>
+              <Progress value={expProgress} className="h-[7px]" />
               <span className="flex items-center gap-1">
                 <img src="/gold.png" alt="gold" className="h-3"/>
-                <p className="text-label tracking-wider">500,000</p>
+                <p className="text-label tracking-wider">{playerInfo?.gold?.toLocaleString() || 0}</p>
               </span>
             </div>
           </SidebarMenuButton>
         </SidebarMenuItem>        
       </SidebarMenu>
     </SidebarGroup>
-  )
-}
+  );
+} 
