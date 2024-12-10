290561ad5b3c4432a7dc0fe3e110bd1b9e1b4654
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-10 11:35:43 +0800

    feat: added user path

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const authRouter = require("./auth");
 const helloRouter = require("./hello");
 const playerRouter = require("./player")
+const userRouter = require("./user");
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/hello", helloRouter);
 rootRouter.use("/player", playerRouter);
+rootRouter.use("/user", userRouter);
 
 module.exports = rootRouter;
