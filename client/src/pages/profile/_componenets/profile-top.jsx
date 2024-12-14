d76a6a988df4ffd07961264b98408cf77d34cfbd
helpmegetoutofthiscodinghell <117290310+helpmegetoutofthiscodinghell@users.noreply.github.com>
2024-12-14 23:36:32 +0800

    feat: added items section

--- client/src/pages/profile/_componenets/profile-top.jsx
+++ client/src/pages/profile/_componenets/profile-top.jsx
@@ -32,7 +32,7 @@ export default function ProfileTop(){
     const expProgress = (playerData?.experience + 270 || 0) / (playerData?.level * 1000) * 100;
 
     return (
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 border border-red-500 w-7/12">
             <div className="flex flex-col gap-2">
                 <span className="text-label tracking-wider text-5xl">
                     <strong>{userData?.name || "Unknown"}</strong>
@@ -44,17 +44,16 @@ export default function ProfileTop(){
                 </span>
             </div>
 
+            <div className="flex flex-row border border-red-500">
 
-            <div className="flex flex-row border border-red-500 w-2/3">
-
-                <div className="flex flex-col gap-2 w-2/5 border border-red-500">
+                <div className="flex flex-col gap-2 w-3/5 border border-red-500">
                     <span className="text-3xl font-bold">Level</span>
                     <span className="text-label tracking-wider text-xl text-gray-500">
-                        Level: {playerData?.level || 1}
+                        Level: {playerData?.adventurerRank || 1}
                     </span>
                     <div className="inline-flex relative object-center">
                         <Progress value={expProgress} className="h-[12px] w-9/12"/> 
-                        <div className="absolute -inset-y-1 right-1">
+                        <div className="absolute -inset-y-1.5 right-1">
                             {playerData?.experience} / {playerData?.level * 1000}
                         </div>
                     </div>
@@ -80,6 +79,27 @@ export default function ProfileTop(){
                         </div>
                         <span className="text-label tracking-wider text-lg text-gray-500">
                             Total Net Worth:
+                        </span>   
+                        <div className="inline-flex">
+                            <img src="chinese-coin.png" className="h-5"/> {playerData?.gold?.toLocaleString() || 0}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-2 w-2/5 border border-red-500">
+                    <h2 className="text-3xl font-bold">Items</h2>
+                    <div className="flex flex-col gap-2">
+                        <div className="flex flex-col">
+                            <span className="text-label tracking-wider text-base text-gray-500">
+                                Unique:
+                            </span>   
+                            <span className="text-label tracking-wider text-base text-gray-500">
+                                Total:
+                            </span>   
+                        </div>
+                        
+                        <span className="text-label tracking-wider text-lg text-gray-500">
+                            Valiue:
                         </span>   
                         <div className="inline-flex">
                             <img src="chinese-coin.png" className="h-5"/> {playerData?.gold?.toLocaleString() || 0}
