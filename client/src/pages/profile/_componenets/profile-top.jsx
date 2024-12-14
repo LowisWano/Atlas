b11accdf21eee3fcf7913851eef6c7cf2ae7ed99
helpmegetoutofthiscodinghell <117290310+helpmegetoutofthiscodinghell@users.noreply.github.com>
2024-12-15 02:09:25 +0800

    nit: made it prettier

--- client/src/pages/profile/_componenets/profile-top.jsx
+++ client/src/pages/profile/_componenets/profile-top.jsx
@@ -2,12 +2,7 @@ import { usePlayer } from "@/queries/usePlayer";
 import LoadingSpinner from "@/components/custom-ui/loading-spinner";
 import { Progress } from "@/components/ui/progress"
 import { useItems } from "@/queries/useItems";
-
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-} from "@/components/ui/avatar"
+import { SquarePen } from 'lucide-react';
 
 export default function ProfileTop(){
     const { getPlayerData } = usePlayer();
@@ -38,77 +33,78 @@ export default function ProfileTop(){
     const expProgress = (playerData?.experience + 270 || 0) / (playerData?.level * 1000) * 100;
 
     return (
-        <div className="flex flex-row justify-center gap-5 justify-self-center">
+        <div className="flex flex-row justify-center gap-5">
             
             <div className="flex flex-col w-7/12 gap-3">
 
                 <div className="flex flex-col gap-3 ">
-                    <span className="text-label tracking-wider text-5xl">
-                        <strong>{userData?.name || "Unknown"}</strong>
+                    <span className="inline-flex text-label tracking-wider text-4xl gap-3">
+                        <strong>{userData?.name || "Unknown"}</strong> 
+                        <div className="h-7 w-7 rounded-full flex justify-center items-center bg-slate-600 cursor-pointer"><SquarePen className="h-4 w-4"/></div>
                     </span>
-                    <span className="text-label tracking-wider text-xl text-gray-500">
+                    <blockquote className="hidden md:block italic text-base text-muted-foreground mt-2">
                         Coolest dude in the world!
-                    </span>
+                    </blockquote>
                 </div>
 
                 <div className="flex flex-row space-x-10">
                     <div className="flex flex-col gap-2 w-6/12">
-                        <span className="text-3xl font-bold">Level</span>
-                        <span className="text-label tracking-wider text-xl text-gray-500">
+                        <span className="text-xl font-bold">Level</span>
+                        <span className="text-label tracking-wider text-base text-gray-500">
                             Level: {playerData?.level || 1}
                         </span>
                         <div className="inline-flex relative object-center">
                             <Progress value={expProgress} className="h-[12px] w-9/12"/> 
-                            <div className="absolute -inset-y-1.5 right-1">
+                            <div className="absolute -inset-y-1 right-1 text-sm">
                                 {playerData?.experience} / {playerData?.level * 1000}
                             </div>
                         </div>
-                        <span className="text-label tracking-wider text-xl text-gray-500">
+                        <span className="text-label tracking-wider text-base text-gray-500">
                             Rank: {playerData?.adventurerRank || 1}
                         </span>
                         <div className="inline-flex relative object-center">
                             <Progress value={expProgress} className="h-[12px] w-9/12"/> 
-                            <div className="absolute -inset-y-1 right-1">
+                            <div className="absolute -inset-y-1 right-1 text-sm">
                                 {playerData?.experience} / {playerData?.level * 1000}
                             </div>
                         </div>
                     </div>
 
                     <div className="flex flex-col gap-2 w-3/12">
-                        <h2 className="text-3xl font-bold">Wealth</h2>
+                        <h2 className="text-xl font-bold">Wealth</h2>
                         <div className="flex flex-col gap-2">
-                            <span className="text-label tracking-wider text-lg text-gray-500">
+                            <span className="text-label tracking-wider text-base text-gray-500">
                                 Wallet:
                             </span>   
-                            <div className="inline-flex gap-2">
-                                <img src="chinese-coin.png" className="h-5"/> {playerData?.gold?.toLocaleString() || 0}
+                            <div className="inline-flex gap-2 relative">
+                                <img src="chinese-coin.png" className="h-3"/> <span className="absolute -inset-y-1 left-5 text-sm"> {playerData?.gold?.toLocaleString() || 0} </span>
                             </div>
-                            <span className="text-label tracking-wider text-lg text-gray-500">
+                            <span className="text-label tracking-wider text-base text-gray-500">
                                 Total Net Worth:
                             </span>   
-                            <div className="inline-flex gap-2">
-                                <img src="chinese-coin.png" className="h-5"/> {playerData?.gold?.toLocaleString() || 0}
+                            <div className="inline-flex gap-2 relative">
+                                <img src="chinese-coin.png" className="h-3"/> <span className="absolute -inset-y-1 left-5 text-sm"> {playerData?.gold?.toLocaleString() || 0} </span>
                             </div>
                         </div>
                     </div>
 
-                    <div className="flex flex-col gap-2  w-3/12">
-                        <h2 className="text-3xl font-bold">Items</h2>
+                    <div className="flex flex-col gap-2 w-3/12">
+                        <h2 className="text-xl font-bold">Items</h2>
                         <div className="flex flex-col gap-2">
                             <div className="flex flex-col">
                                 <span className="text-label tracking-wider text-base text-gray-500">
-                                    Unique:
+                                    Unique: 0
                                 </span>   
-                                <span className="text-label tracking-wider text-base text-gray-500">
-                                    Total:
+                                <span className="text-label tracking-wider text-sm text-gray-500">
+                                    Total: 0
                                 </span>   
                             </div>
                             
-                            <span className="text-label tracking-wider text-lg text-gray-500">
-                                Valiue:
+                            <span className="text-label tracking-wider text-base text-gray-500">
+                                Value:
                             </span>   
-                            <div className="inline-flex gap-2">
-                                <img src="chinese-coin.png" className="h-5"/> {playerData?.gold?.toLocaleString() || 0}
+                            <div className="inline-flex gap-2 relative">
+                                <img src="chinese-coin.png" className="h-3"/> <span className="absolute -inset-y-1 left-5 text-sm"> {playerData?.gold?.toLocaleString() || 0} </span>
                             </div>
                         </div>
                     </div>
@@ -117,8 +113,8 @@ export default function ProfileTop(){
 
             </div>
 
-            <div className="flex justify-center items-center">
-                <div className="h-96">
+            <div className="flex justify-center items-center cursor-pointer" onClick={""}>
+                <div className="h-64">
                     <img
                         src={playerInfo?.profilePic || "https://github.com/shadcn.png"}
                         alt="avatar"
