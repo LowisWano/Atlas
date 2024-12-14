5e9ba682506e3d21673009c99734525b33087178
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-14 14:06:19 +0800

    feat: added player services

---
+++ client/src/services/player.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/player";
+const userUrl = "http://localhost:3001/api/user";
+
+export const getPlayerInfo = async (playerId, token) => {
+  const response = await axios.get(`${baseUrl}/${playerId}/profile`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const getUserInfo = async (playerId, token) => {
+  const response = await axios.get(`${userUrl}/${playerId}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
