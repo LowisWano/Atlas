f9724f54ed3fc8521d00f96b9b2181e6c8f3fa69
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-15 12:55:35 +0800

    feat: added POST/GET path for 5000 gold achivement

--- backend/src/routes/achievements.js
+++ backend/src/routes/achievements.js
@@ -8,5 +8,6 @@ achievementsRouter.post("/:id/check-first-quest", tokenValidator, achievements.c
 achievementsRouter.post("/:id/check-first-main-quest", tokenValidator, achievements.checkFirstMainQuestAchievementController);
 achievementsRouter.post("/:id/check-first-daily-quest", tokenValidator, achievements.checkFirstDailyQuestAchievementController);
 achievementsRouter.post("/:id/check-first-purchase", tokenValidator, achievements.checkFirstPurchaseAchievementController);
+achievementsRouter.post("/:id/check-5000-gold", tokenValidator, achievements.check5000GoldAchievementController);
 
 module.exports = achievementsRouter;
