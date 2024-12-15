e385e37fd7d46adbf95cedea1fa4dc416ba651b2
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-15 10:34:17 +0800

    feat: added new GET/POST path for First Main quest Achivement

--- backend/src/routes/achievements.js
+++ backend/src/routes/achievements.js
@@ -5,5 +5,6 @@ const { tokenValidator } = require("../utils/middleware");
 achievementsRouter.get("/list", tokenValidator, achievements.getAchievementsListController);
 achievementsRouter.get("/:id/user-achievements", tokenValidator, achievements.getUserAchievementsController);
 achievementsRouter.post("/:id/check-first-quest", tokenValidator, achievements.checkFirstQuestAchievementController);
+achievementsRouter.post("/:id/check-first-main-quest", tokenValidator, achievements.checkFirstMainQuestAchievementController);
 
 module.exports = achievementsRouter;
