5589db33475ee8a265683c0ea641a564301f4a99
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-15 09:35:42 +0800

    feat: added controller for first Achivement checker

--- backend/src/controllers/achievements.controller.js
+++ backend/src/controllers/achievements.controller.js
@@ -1,21 +1,29 @@
 const {
-    getAchievements,
-    getPlayerAchievements,
-  } = require("../services/achievements.service");
-  require("express-async-errors");
-  
-  const getAchievementsListController = async (req, res) => {
-    const achievements = await getAchievements();
-    res.json(achievements);
-  };
-   
-  const getUserAchievementsController = async (req, res) => {
-    const playerId = Number(req.params.id);
-    const playerAchievements = await getPlayerAchievements(playerId);
-    res.json(playerAchievements);
-  };
-  
-  module.exports = {
-    getAchievementsListController,
-    getUserAchievementsController,
-  };  
+  getAchievements,
+  getPlayerAchievements,
+  checkAndCreateFirstQuestAchievement,
+} = require("../services/achievements.service");
+require("express-async-errors");
+
+const getAchievementsListController = async (req, res) => {
+  const achievements = await getAchievements();
+  res.json(achievements);
+};
+
+const getUserAchievementsController = async (req, res) => {
+  const playerId = Number(req.params.id);
+  const playerAchievements = await getPlayerAchievements(playerId);
+  res.json(playerAchievements);
+};
+
+const checkFirstQuestAchievementController = async (req, res) => {
+  const playerId = Number(req.params.id);
+  await checkAndCreateFirstQuestAchievement(playerId);
+  res.status(200).send("Achievement check completed");
+};
+
+module.exports = {
+  getAchievementsListController,
+  getUserAchievementsController,
+  checkFirstQuestAchievementController,
+};
