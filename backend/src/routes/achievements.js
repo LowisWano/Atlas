9bc2325e6dd1bfd8ce07dd6ecfcdca54046ce8e2
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-15 11:13:47 +0800

    feat: new GET/POST path for first pruchase achivement

--- backend/src/routes/achievements.js
+++ backend/src/routes/achievements.js
@@ -7,5 +7,6 @@ achievementsRouter.get("/:id/user-achievements", tokenValidator, achievements.ge
 achievementsRouter.post("/:id/check-first-quest", tokenValidator, achievements.checkFirstQuestAchievementController);
 achievementsRouter.post("/:id/check-first-main-quest", tokenValidator, achievements.checkFirstMainQuestAchievementController);
 achievementsRouter.post("/:id/check-first-daily-quest", tokenValidator, achievements.checkFirstDailyQuestAchievementController);
+achievementsRouter.post("/:id/check-first-purchase", tokenValidator, achievements.checkFirstPurchaseAchievementController);
 
 module.exports = achievementsRouter;
