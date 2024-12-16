9085ecd8cea80b999710185c95365b01c073a478
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-16 17:19:19 +0800

    feat: updated profile display and etc

--- client/src/pages/profile/_componenets/profile-top.jsx
+++ client/src/pages/profile/_componenets/profile-top.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
-import { usePlayer } from "@/queries/usePlayer";
-import LoadingSpinner from "@/components/custom-ui/loading-spinner";
-import { Progress } from "@/components/ui/progress";
-import { useItems } from "@/queries/useItems";
-import { SquarePen } from "lucide-react";
-import EditProfileModal from "./edit-profile-modal";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { useState } from 'react';
+import { usePlayer } from '@/queries/usePlayer';
+import LoadingSpinner from '@/components/custom-ui/loading-spinner';
+import { Progress } from '@/components/ui/progress';
+import { useItems } from '@/queries/useItems';
+import { SquarePen } from 'lucide-react';
+import EditProfileModal from './edit-profile-modal';
 
 export default function ProfileTop() {
   const [open, setOpen] = useState(false);
@@ -37,9 +30,6 @@ export default function ProfileTop() {
   const userData = userInfo;
   const playerItems = itemsData.data;
 
-//   console.log("playerData:", playerData);
-//   console.log("userData:", userData);
-
   const expProgress = (playerData?.experience || 0) / (playerData?.level * 1000) * 100;
   const rankProgress = (playerData?.rankPoints || 0) / (playerData?.level * 1000) * 100;
 
@@ -52,24 +42,14 @@ export default function ProfileTop() {
     <div className="flex flex-col md:flex-row justify-center gap-5 p-5">
       <div className="flex flex-col w-full md:w-7/12 gap-3">
         <div className="flex flex-col gap-3">
-          {/* Edit Modal Button */}
           <span className="inline-flex text-lg md:text-4xl tracking-wider gap-3">
-            <strong>{userData?.name || "Unknown"}</strong>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <div
-                    className="h-7 w-7 rounded-full flex justify-center items-center bg-slate-600 cursor-pointer"
-                    onClick={editModalOpener}
-                  >
-                    <SquarePen className="h-4 w-4" />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Edit Profile</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+            <strong>{userData?.name || 'Unknown'}</strong>
+            <div
+              className="h-7 w-7 rounded-full flex justify-center items-center bg-slate-600 cursor-pointer"
+              onClick={editModalOpener}
+            >
+              <SquarePen className="h-4 w-4" />
+            </div>
           </span>
           <blockquote className="block italic text-sm md:text-base text-muted-foreground mt-2">
             {playerData?.bio}
@@ -79,7 +59,7 @@ export default function ProfileTop() {
         <div className="flex justify-center items-center md:hidden">
           <div className="h-40 w-40 md:h-64 md:w-64 rounded-xl overflow-hidden">
             <img
-              src={playerInfo?.profilePic || "https://github.com/shadcn.png"}
+              src={playerInfo?.profilePic || 'https://github.com/shadcn.png'}
               alt="avatar"
               className="h-full w-full object-cover"
             />
@@ -149,14 +129,13 @@ export default function ProfileTop() {
       <div className="justify-center items-center hidden md:flex">
         <div className="h-40 w-40 md:h-64 md:w-64 rounded-xl overflow-hidden">
           <img
-            src={playerInfo?.profilePic || "https://github.com/shadcn.png"}
+            src={playerInfo?.profilePic || 'src/assets/profile/noPfp.jpg'}
             alt="avatar"
             className="h-full w-full object-cover"
           />
         </div>
       </div>
 
-      {/* Edit Profile Modal */}
       <EditProfileModal open={open} setOpen={setOpen} playerData={playerData} userData={userData} />
     </div>
   );
